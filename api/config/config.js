28d0d0c3edbec4168b5b93d72aeb31140ab64872
Pixel Hermit <145558376+A-nirvana@users.noreply.github.com>
2024-04-03 22:34:51 +0530

    Update config.js

--- api/config/config.js
+++ api/config/config.js
@@ -1,5 +1,5 @@
 const config = {
-    db: "bill-Buddy",
+    db: "NGO-dash",
     port: parseInt(process.env.PORT ?? "3000"),
     baseURL:
       process.env.SERVER_BASE_URL || `http://localhost:${process.env.PORT}`,
